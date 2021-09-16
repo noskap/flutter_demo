bc4a5f822e8028ddf812262c50fa7cdfd7372cfe
Tom @ Bingus Bongus <noskap@outlook.com>
2021-09-17 08:57:44 +1000

    Added animation

--- flutter_demo/lib/core/demo-app/screens/store/common/widgets/products.widget.dart
+++ flutter_demo/lib/core/demo-app/screens/store/common/widgets/products.widget.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_demo/core/demo-app/screens/store/common/services/dummy-products.service.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 
 import '../mon.dart';
 
@@ -20,57 +21,63 @@ class Products extends StatelessWidget {
             stream: DummyProductsService().getMons(),
             builder: (BuildContext context, AsyncSnapshot<List<Mon>> snapshot) {
               if (snapshot.hasData) {
-                return ListView.builder(
-                  itemCount: snapshot.data?.length ?? 0,
-                  padding: EdgeInsets.all(0),
-                  itemBuilder: (BuildContext context, int index) {
-                    final Mon mon = snapshot.data![index];
-                    return Card(
-                      semanticContainer: true,
-                      clipBehavior: Clip.antiAliasWithSaveLayer,
-                      child: SizedBox(
-                        height: 110,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Column(
-                            children: [
-                              Row(
+                return AnimationLimiter(
+                  child: ListView.builder( // TODO this aint working right
+                    itemCount: snapshot.data?.length ?? 0,
+                    padding: EdgeInsets.all(0),
+                    itemBuilder: (BuildContext context, int index) {
+                      final Mon mon = snapshot.data![index];
+                      return AnimationConfiguration.staggeredList(
+                        position: index,
+                        duration: Duration(milliseconds: 500),
+                        child: Card(
+                          semanticContainer: true,
+                          clipBehavior: Clip.antiAliasWithSaveLayer,
+                          child: SizedBox(
+                            height: 110,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
                                 children: [
-                                  Text('#'),
-                                  Text(mon.pokemonId.toString()),
-                                ],
-                              ),
-                              Row(
-                                children: [
-                                  Padding(
-                                    padding: const EdgeInsets.only(top: 14, bottom: 14),
-                                    child: Text(mon.pokemonName),
+                                  Row(
+                                    children: [
+                                      Text('#'),
+                                      Text(mon.pokemonId.toString()),
+                                    ],
                                   ),
-                                  Spacer(),
-                                  Image.network(
-                                    DummyProductsService().getImageUrl(mon.formatNumber()),
-                                    width: 50,
-                                    height: 50,
+                                  Row(
+                                    children: [
+                                      Padding(
+                                        padding: const EdgeInsets.only(top: 14, bottom: 14),
+                                        child: Text(mon.pokemonName),
+                                      ),
+                                      Spacer(),
+                                      Image.network(
+                                        DummyProductsService().getImageUrl(mon.formatNumber()),
+                                        width: 50,
+                                        height: 50,
+                                      ),
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [
+                                      Text(r'$'),
+                                      Text(Random().nextInt(mon.pokemonId > 10 ? mon.pokemonId : 10).toString()),
+                                    ],
                                   ),
                                 ],
                               ),
-                              Row(
-                                children: [
-                                  Text(r'$'),
-                                  Text(Random().nextInt(mon.pokemonId > 10 ? mon.pokemonId : 10).toString()),
-                                ],
-                              ),
-                            ],
+                            ),
                           ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                          ),
+                          elevation: 5,
+                          margin: EdgeInsets.all(5),
                         ),
-                      ),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(5.0),
-                      ),
-                      elevation: 5,
-                      margin: EdgeInsets.all(5),
-                    );
-                  },
+                      );
+                    },
+                  ),
                 );
               } else {
                 return SizedBox();
